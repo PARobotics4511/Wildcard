835aaa73533bf4a1bdfd3fce430b0834de492df7
William Walcher <wjwalcher@gmail.com>
2017-02-04 14:29:51 -0600

    Testing vision

--- src/org/usfirst/frc/team4511/robot/commands/Align.java
+++ src/org/usfirst/frc/team4511/robot/commands/Align.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team4511.robot.commands;
 import org.usfirst.frc.team4511.robot.Robot;
 import org.usfirst.frc.team4511.robot.subsystems.DriveTrain;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -14,8 +15,11 @@ public class Align extends Command {
 	Pair<Double, Boolean> xPosition;
 	final double POS_THRESHOLD = .1;
 	final double NEG_THRESHOLD = -.1;
+	final double TARGET_DISTANCE = 10;
 	final double driveSpeed = .4;
+	final double turnSpeed = .3;
 	boolean shouldExit;
+	boolean commandDone;
 	public Align() {
     	requires(Robot.vision);
     	requires(Robot.drivetrain);
@@ -41,16 +45,24 @@ public class Align extends Command {
     	if(xPosition.t < POS_THRESHOLD && xPosition.t >  NEG_THRESHOLD){
     		DriveTrain.drive(driveSpeed, driveSpeed);
     	}else if(xPosition.t > POS_THRESHOLD){
-    		DriveTrain.drive(.3, .4);
+    		DriveTrain.drive(turnSpeed, driveSpeed);
     	}else if(xPosition.t < NEG_THRESHOLD){
-    		DriveTrain.drive(.4, .3);
+    		DriveTrain.drive(driveSpeed, turnSpeed);
+    	}else if(distance.t <= TARGET_DISTANCE){
+    		DriveTrain.drive(-driveSpeed, -driveSpeed);
+    		Timer.delay(2);
+    		DriveTrain.stop();
+    		commandDone = true;
     	}
     	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        if(commandDone || shouldExit){
+        	return true;
+        }
+    	return false;
     }
 
     // Called once after isFinished returns true
